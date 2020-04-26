61f02648c479049ea583ce6c6b865ae66367e6ff
LiamKrieling <lmk310500@gmail.com>
2020-04-27 01:39:24 +0200

    added gitignore

--- main.c
+++ main.c
@@ -1,14 +1,9 @@
 #include "global.h"
 
-
-
-// extern registry *reg;
-
 int main(void){
 	t_rd *test1 = NULL;
 	t_hub *hub = NULL;
 	
-	// init_registry();
 	read_in(&test1);
 	init_build(&hub, test1);
 	printf("done !\n");
