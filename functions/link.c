83470f7700db2eed2f2de7795a8be6f97f2a8637
LiamKrieling <lmk310500@gmail.com>
2020-04-28 11:36:44 +0200

    removed some console los

--- functions/link.c
+++ functions/link.c
@@ -37,20 +37,20 @@ int connector(t_hub *hub, t_room *room, t_room *next_room){
 	t_link *new = malloc_link();
 	t_link *temp = room->links;
 	new->linked_room = next_room;
-	printf("####### connecting ########\n");
-	printf("binding %s:%p to %d:%p\n", room->name, room, next_room->id, next_room);
+	// printf("####### connecting ########\n");
+	// printf("binding %s:%p to %d:%p\n", room->name, room, next_room->id, next_room);
 	if (!temp){
-		printf("first node:%p attaching %d:%p to links of %d\n", new,new->linked_room->id, new->linked_room, room->id);
+		// printf("first node:%p attaching %d:%p to links of %d\n", new,new->linked_room->id, new->linked_room, room->id);
 		room->links = new;
 	}
 	else {
 		while (temp->next)
 			temp = temp->next;
 		temp->next = new;
-		printf("secon node:%p attaching %d:%p to links of %d\n", new,new->linked_room->id, new->linked_room, room->id);
+		// printf("secon node:%p attaching %d:%p to links of %d\n", new,new->linked_room->id, new->linked_room, room->id);
 	}
-	printf("%s's", room->name);
-	print_links(room->links);
-	printf("\n####### connected #########\n\n\n");
+	// printf("%s's", room->name);
+	// print_links(room->links);
+	// printf("\n####### connected #########\n\n\n");
 	return (1);
 }
